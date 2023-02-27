459c0e486af7b966edb495a40d74a41a140c9474
Mohammed654321 <126425351+Mohammed654321@users.noreply.github.com>
2023-02-27 20:22:29 +0300

    Create Q_Five.java

---
+++ HW/Lap_Five/Q_Five.java
@@ -0,0 +1,18 @@
+public class Q_Five {
+    public void half(CircularlyLinkedList list1, CircularlyLinkedList list2){
+        if(!isEmpty())
+            if(size % 2 == 0){
+                int count = 1;
+                while(count <= size/2){
+                    tail = tail.next;
+                    list1.addLast(tail.element);
+                    count++;
+                }
+                while(count <= size){
+                    tail = tail.next;
+                    list2.addLast(tail.element);
+                    count++;
+                }
+            }
+    }
+}
