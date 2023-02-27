861c120206a9dd32e66ddb98d2e18b2db6595f70
Mohammed654321 <126425351+Mohammed654321@users.noreply.github.com>
2023-02-27 19:51:29 +0300

    Create Lab_ThreeQ_One.java

---
+++ HW/Lab_ThreeQ_One.java
@@ -0,0 +1,10 @@
+public class Q_One {
+    public static void main(String[] args) {
+        
+        int[] arr = {10,11,12,13,14,15,16};
+        int[] arr2 = new int[7];
+        
+        for(int i = 0; i < arr2.length; i++)
+            arr2[i] = arr[i];
+    }
+}
