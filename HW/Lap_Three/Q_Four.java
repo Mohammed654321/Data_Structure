0dcd8f4d543a88588e73dd0d80f7241ebc69a25d
Mohammed654321 <126425351+Mohammed654321@users.noreply.github.com>
2023-02-27 20:01:16 +0300

    Create Q_Four.java

---
+++ HW/Lap_Three/Q_Four.java
@@ -0,0 +1,28 @@
+public class Q_Four {
+    
+    public static void main(String[] args) {
+        
+        int[] arr = {1,2,3,4,5};
+        int[] arr2 = {6,7,8,9,10,11,12};
+        boolean equal = true;
+        
+        if(arr == arr2){
+            System.out.println("equals");
+        }
+        else if(arr.length == arr2.length) {
+            for(int i=0; i < arr.length; i++){
+                if(arr[i] != arr2[i]){
+                    equal = true;
+                    break;
+                }
+                else
+                    equal = false;
+            }
+        }
+        if(equal)
+            System.out.println("not equals");
+        else
+            System.out.println("equals");
+        
+    }
+}
