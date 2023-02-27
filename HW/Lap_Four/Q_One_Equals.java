8e4d232e6b4f1d3650a476276172a38570681ba1
Mohammed654321 <126425351+Mohammed654321@users.noreply.github.com>
2023-02-27 20:17:13 +0300

    Create Q_One_Equals.java

---
+++ HW/Lap_Four/Q_One_Equals.java
@@ -0,0 +1,12 @@
+public class Q_One_Equals {
+    public boolean equals(Node n, Node n2){
+        if(n == n2){
+            if(n.next == n2.next){
+                if(n.element == n2.element){
+                    return true;
+                }
+            }
+        }
+            return false;   
+    }
+}
