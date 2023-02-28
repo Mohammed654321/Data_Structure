c1e786fecc5d2239a8a9e9bdcc30c0fbc25f0fd9
Mohammed654321 <126425351+Mohammed654321@users.noreply.github.com>
2023-02-28 17:55:23 +0300

    Create Q_Three.java

---
+++ HW/Lap_Seven/Q_Three.java
@@ -0,0 +1,15 @@
+public class Q_Three {
+    // the first step is create method in class SinglyLinkedList //
+    public void Concatenate(SinglyLinkedList list) {
+        tail.setNext(list.head);
+        tail = list.tail;
+        size += list.size;
+        list.size = 0;
+    }
+    
+    // then create method in class SinlyQueue //
+    public void signatureConcatenate(SinglyQueue Q2){
+        this.list.Concatenate(Q2.list);
+    }
+                         
+}
